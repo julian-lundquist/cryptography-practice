13e6b7b6f841d086f6be982eb0dbaba4d011e4e9
julian-lundquist <julian.j.lundquist@gmail.com>
2021-04-20 16:13:12 -0500

    added small change reguarding naming convention

--- FrequencyAnalysis.py
+++ FrequencyAnalysis.py
@@ -24,11 +24,11 @@ class Attack:
         self.cipher_chars_left = "abcdefghijklmnopqrstuvwxyz"
         self.key = {}
         self.freq = {}
-        self.freq_eng = {'a': 0.0817, 'b': 0.0150, 'c': 0.0278, 'd': 0.0425, 'e': 0.1270, 'f': 0.0223,
-                         'g': 0.0202, 'h': 0.0609, 'i': 0.0697, 'j': 0.0015, 'k': 0.0077, 'l': 0.0403,
-                         'm': 0.0241, 'n': 0.0675, 'o': 0.0751, 'p': 0.0193, 'q': 0.0010, 'r': 0.0599,
-                         's': 0.0633, 't': 0.0906, 'u': 0.0276, 'v': 0.0098, 'w': 0.0236, 'x': 0.0015,
-                         'y': 0.0197, 'z': 0.0007}
+        self.freq_english = {'a': 0.0817, 'b': 0.0150, 'c': 0.0278, 'd': 0.0425, 'e': 0.1270, 'f': 0.0223,
+                             'g': 0.0202, 'h': 0.0609, 'i': 0.0697, 'j': 0.0015, 'k': 0.0077, 'l': 0.0403,
+                             'm': 0.0241, 'n': 0.0675, 'o': 0.0751, 'p': 0.0193, 'q': 0.0010, 'r': 0.0599,
+                             's': 0.0633, 't': 0.0906, 'u': 0.0276, 'v': 0.0098, 'w': 0.0236, 'x': 0.0015,
+                             'y': 0.0197, 'z': 0.0007}
         self.mappings = {}
 
     def calculate_freq(self, cipher):
@@ -56,7 +56,7 @@ class Attack:
         for cipher_char in self.alphabet:
             map = {}
             for plain_char in self.alphabet:
-                map[plain_char] = round(abs(self.freq[cipher_char] - self.freq_eng[plain_char]), 4)
+                map[plain_char] = round(abs(self.freq[cipher_char] - self.freq_english[plain_char]), 4)
             self.mappings[cipher_char] = sorted(map.items(), key=operator.itemgetter(1))
 
     def set_key_mapping(self, cipher_char, plain_char):
