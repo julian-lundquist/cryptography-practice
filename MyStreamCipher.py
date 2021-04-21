7c37f5df5a57aa8f1cf480716dc2a9777fda8bf1
julian-lundquist <julian.j.lundquist@gmail.com>
2021-04-21 16:18:39 -0500

    flipping random character byte code in transmission of a stream cipher to change the end result

--- MyStreamCipher.py
+++ MyStreamCipher.py
@@ -1,3 +1,5 @@
+import random
+
 class KeyStream:
     def __init__(self, key=1):
         self.next = key
@@ -12,12 +14,24 @@ class KeyStream:
 def encrypt(key, message):
     return bytes([message[i] ^ key.get_key_byte() for i in range(len(message))])
 
+def trasmit(cipher, chance):
+    b = []
+    for c in cipher:
+        if random.randrange(0, chance) == 0:
+            c = c ^ 2**random.randrange(0, 8)
+        b.append(c)
+    return bytes(b)
+
 # can change the key value by inputing in the parameters in KeyStream
 key = KeyStream(10)
 message = "Hello, World!".encode()
 cipher = encrypt(key, message)
 print(cipher)
 
+cipher = trasmit(cipher, 5)
+print(cipher)
+
 key = KeyStream(10)
 message = encrypt(key, cipher)
+
 print(message)
