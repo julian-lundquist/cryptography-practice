b94adc9622d7f474eba71d6a8c38802228638f2f
julian-lundquist <julian.j.lundquist@gmail.com>
2021-04-23 14:33:47 -0500

    added example of sha512 encryption using decoded salt and encoded password to produce the final hash value and verify the match to ensure if the password is correct

---
+++ ShadowFiles.py
@@ -0,0 +1,11 @@
+import hashlib
+import base64
+
+iterations = 45454
+salt = base64.b64decode("6VuJKkHVTdDelbNMPBxzw7INW2NkYlR/LoW4OL7kVAI=".encode())
+# SALTED-SHA512-PBKDF2
+
+password = "password".encode()
+
+value = hashlib.pbkdf2_hmac("sha512", password, salt, iterations, dklen=128)
+print(base64.b64encode(value))
