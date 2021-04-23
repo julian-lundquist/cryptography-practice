a8b2743002aa086ec6d26bf7a84e00e342d9f9ef
julian-lundquist <julian.j.lundquist@gmail.com>
2021-04-23 15:03:46 -0500

    added examples of sha512 hash validation

--- ShadowFiles.py
+++ ShadowFiles.py
@@ -3,9 +3,20 @@ import base64
 
 iterations = 45454
 salt = base64.b64decode("6VuJKkHVTdDelbNMPBxzw7INW2NkYlR/LoW4OL7kVAI=".encode())
-# SALTED-SHA512-PBKDF2
+#salt = "".encode()
+validation = "SALTED-SHA512-PBKDF2"
 
 password = "password".encode()
 
+# Alice value
 value = hashlib.pbkdf2_hmac("sha512", password, salt, iterations, dklen=128)
-print(base64.b64encode(value))
+entropy = base64.b64encode(value)
+print("Alice: ", validation, iterations, entropy)
+
+# Bob value
+salt = base64.b64decode("6VuJKkHVTdDelbL7kVAI=".encode())
+password = "password".encode()
+value = hashlib.pbkdf2_hmac("sha512", password, salt, iterations, dklen=128)
+entropy = base64.b64encode(value)
+print("Bob: ", validation, iterations, entropy)
+
