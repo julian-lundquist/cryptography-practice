df17769a0975feab52f1f7647b8548268508025c
julian-lundquist <julian.j.lundquist@gmail.com>
2021-04-23 16:22:11 -0500

    the message input cannot be modified by eve without bob knowing that it happened; validation of the hash values will fail because no match

--- HMAC.py
+++ HMAC.py
@@ -1,6 +1,11 @@
 import hashlib
 import base64
 
+def modify(m):
+    l = list(m)
+    l[0] = l[0] ^ 1
+    return bytes(l)
+
 # Alice and Bob share a secret key
 secret_key = "secret key".encode()
 
@@ -12,6 +17,10 @@ sha256.update(m)
 hmac = base64.b64encode(sha256.digest())
 
 print(m, hmac)
+
+# Eve modifying the message maliciously
+m = modify(m)
+print(m)
 
 # Bob receives and validates the HMAC
 sha256 = hashlib.sha256()
